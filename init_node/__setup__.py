f25cfd13edae3532813a9a3db585c1462bd48980
DmMeta <dimetaxakis@gmail.com>
2023-11-12 18:11:06 +0200

    Added watchdog dep. | configured setup for testing
    Co-authored-by: Sofosss <argsofos.1@gmail.com>

--- init_node/__setup__.py
+++ init_node/__setup__.py
@@ -42,18 +42,18 @@ class ChordInitialization:
 
             for key,value in containers.items():
                 if key != self.container_id:
-                    network.append((value["Name"],value["IPv4Address"]))
+                    network.append((value["Name"],value["IPv4Address"].split("/")[0]))
 
         except docker.errors.APIError as e:
             print(f"The {self.netname} docker network wasn't found. Critical failure...")
             #return a sorted (ip address) list of nodes
-        return sorted(network, key = lambda x: int(x[1].split(".")[3].split("/")[0]))
+        return sorted(network, key = lambda x: int(x[1].split(".")[3]))
 
     def initialize(self):
         #TODO: check altering the channel each time.
         print(f"Initial hosts in network: {len(self.network)}")
 
-        shuffle(self.network)
+        # shuffle(self.network)
         elected_host = self.network.pop()
         print(f"Election begins...")
         sleep(1.5)
@@ -64,16 +64,18 @@ class ChordInitialization:
         self.hops = client.join(JoinRequest(ip_addr = elected_host[1], init = True)).num_hops
         self.active_chord.append(elected_host)
 
-        for i in range(self.nodes -1):
-            shuffle(self.network)
-            arbitary_node = self.active_chord[randint(0,len(self.active_chord)-1)]
+
+        for i in range(3): #self.nodes -1
+            #shuffle(self.network)
+            arbitary_node = self.active_chord[i] #randint(0,len(self.active_chord)-1)
+            print(arbitary_node)
             elected_host = self.network.pop()
             print(f"[{i}]: Election begins...")
             sleep(0.3)
             print(f"[{i}]: Elected Node for initilization: {elected_host}")
             channel = grpc.insecure_channel(elected_host[0]+":50051")
             client = ChordStub(channel)
-            self.hops = self.hops + client.join(JoinRequest(ip_addr=arbitary_node[1])).num_hops
+            self.hops = self.hops + client.join(JoinRequest(ip_addr = arbitary_node[1])).num_hops
             self.active_chord.append(elected_host)
 
 
@@ -87,6 +89,7 @@ if __name__ == "__main__":
     print(f"Total hops: {chinit.hops}\nNetwork size: {len(chinit.active_chord)}")
     for el in chinit.active_chord:
         print(el)
+    
 
 #print(chinit.netname, chinit.container_id,chinit.network,chinit.nodes)
 # print(type(chinit.network))
